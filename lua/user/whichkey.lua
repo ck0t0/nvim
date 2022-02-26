cbfb94f9bb5acfe63a9ea45c063bb45d1ececcfe
Chris Karol <chris@killerbees.xyz>
2022-02-26 18:11:02 -0500

    more dap keymap config

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -163,6 +163,7 @@ local mappings = {
     name = "DAP",
     b = { "<Cmd>lua require('dap').toggle_breakpoint()<CR>", "Toggle breakpoint" },
     c = { "<Cmd>lua require('dap').continue()<CR>", "Continue" },
+    t = { "<Cmd>lua require('dap').run_to_cursor()<CR>", "Run to Cursor" },
     s = { "<Cmd>lua require('dap').step_over()<CR>", "Step over" },
     i = { "<Cmd>lua require('dap').step_into()<CR>", "Step into" },
     o = { "<Cmd>lua require('dap').step_out()<CR>", "Step out" },
